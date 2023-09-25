9515bdf5a3088622d42eb8dfc1fb070b7513f568
Med Vall <elhadjsidimv@gmail.com>
2023-09-25 11:36:41 +0000

    add new parametre

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,13 +1,14 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "postgres",
-    PASSWORD: "admin",
-    DB: "testdb",
-    dialect: "postgres",
+
+    HOST: process.env.DB_HOST || "localhost",
+    USER: process.env.DB_USER || "postgres",
+    PASSWORD: process.env.DB_PASSWORD || "admin",
+    DB: process.env.DB_NAME || "testdb",
+    dialect: process.env.DB_DIALECT || "postgres",
     pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
+        max: process.env.DB_POOL_MAX || 5,
+        min: process.env.DB_POOL_MIN || 0,
+        acquire: process.env.DB_POOL_ACQUIRE || 30000,
+        idle: process.env.DB_POOL_IDLE || 10000
     }
   };
